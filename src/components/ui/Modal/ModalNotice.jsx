3a2771a974823dff8cbc18de5888cb44ae7a44ac
sgwag6799 <sgwag6799@gmail.com>
2026-07-20 10:22:43 +0900

    feat: 작성완료+ 주석정리

--- src/components/ui/Modal/ModalNotice.jsx
+++ src/components/ui/Modal/ModalNotice.jsx
@@ -4,11 +4,7 @@ import { useModal } from '@/hooks/modal/useModal';
 
 import { cn } from '@/utils/cn';
 
-import ModalBase, {
-  ModalContent,
-  ModalActions,
-  getModalButtonClassName,
-} from './ModalBase';
+import ModalBase, { ModalContent, getModalButtonClassName } from './ModalBase';
 
 /**
  * 단순 안내 팝업 "내용" 컴포넌트. openModal(<ModalNotice .../>)로 호출해서 사용.
@@ -18,7 +14,6 @@ import ModalBase, {
  * openModal(<ModalNotice message={"가입이 완료되었습니다!"} />);
  */
 export default function ModalNotice({
-  icon = null,
   message,
   confirmText = '확인',
   onConfirm,
@@ -32,8 +27,8 @@ export default function ModalNotice({
 
   return (
     <ModalBase>
-      <ModalContent className={cn('flex flex-col items-center pt-6')}>
-        {icon && <div className={cn('mb-3 flex justify-center')}>{icon}</div>}
+      <ModalContent className={cn('flex flex-col items-center pt-20 pb-11')}>
+        {/* {icon && <div className={cn('mb-3 flex justify-center')}>{icon}</div>} */}
         <p
           className={cn(
             'whitespace-pre-line text-center text-16-semibold text-gray-900',
@@ -42,11 +37,7 @@ export default function ModalNotice({
           {message}
         </p>
       </ModalContent>
-      <ModalActions
-        className={cn(
-          'justify-center bd:justify-end pb-7 [&>button]:flex-none!',
-        )}
-      >
+      <div className={cn('flex justify-center bd:justify-end px-25 pb-7')}>
         <button
           type="button"
           className={getModalButtonClassName('primary', 'h-10 px-5')}
@@ -54,7 +45,7 @@ export default function ModalNotice({
         >
           {confirmText}
         </button>
-      </ModalActions>
+      </div>
     </ModalBase>
   );
 }
