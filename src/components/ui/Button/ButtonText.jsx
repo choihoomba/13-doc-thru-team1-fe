1d8fc91d5b39d176c15158f03f215be09a30352b
damdadaxx <hyojudev@gmail.com>
2026-07-22 23:34:21 +0900

    feat: ButtonText 컴포넌트 구현 - Link 기반 텍스트 링크 버튼으로 구현 - 밑줄 스타일 적용

--- src/components/ui/Button/ButtonText.jsx
+++ src/components/ui/Button/ButtonText.jsx
@@ -1,3 +1,27 @@
-export default function ButtonText() {
-  return <div>ButtonText</div>;
+import Link from 'next/link';
+
+import { cn } from '@/utils/cn';
+
+import { BUTTON_BASE_STYLE } from '@/components/ui/Button/buttonStyles';
+
+/** 텍스트 스타일 버튼 */
+export default function ButtonText({
+  className = '',
+  href,
+  text = '',
+  ...props
+}) {
+  return (
+    <Link
+      className={cn(
+        BUTTON_BASE_STYLE,
+        'block text-16-regular underline decoration-solid decoration-auto underline-offset-[3px]',
+        className,
+      )}
+      href={href}
+      {...props}
+    >
+      {text}
+    </Link>
+  );
 }
