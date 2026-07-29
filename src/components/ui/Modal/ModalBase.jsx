c448d217a32c0936b6d95a81e776a9884a8403da
sgwag6799 <sgwag6799@gmail.com>
2026-07-29 12:02:23 +0900

    fix: modal 스크롤바

--- src/components/ui/Modal/ModalBase.jsx
+++ src/components/ui/Modal/ModalBase.jsx
@@ -15,11 +15,19 @@ export default function ModalBase({ children, className }) {
   useOutsideClick(boxRef, closeModal, { closeOnEscape: true });
 
   useEffect(() => {
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
     const prevOverflow = document.body.style.overflow;
+    const prevPaddingRight = document.body.style.paddingRight;
+
     document.body.style.overflow = 'hidden';
+    if (scrollbarWidth > 0) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
 
     return () => {
       document.body.style.overflow = prevOverflow;
+      document.body.style.paddingRight = prevPaddingRight;
     };
   }, []);
 
